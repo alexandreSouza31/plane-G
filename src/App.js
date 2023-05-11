38969b144fab2dcd63d795f6548ebf2b292d0350
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-11 11:55:45 -0300

    will create the logo of the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import "./App.css";
+import Logo from "./img/logo.png";
 function App() {
   return (
     <div className="App">
-      <h1>Pensou em viagem, pensou Plane-G!</h1>
+      <h1>Pensou em viajar?
+        <img src={Logo} style={{ height: 100, width: 100 }} />
+      </h1>
     </div>
   );
 }
