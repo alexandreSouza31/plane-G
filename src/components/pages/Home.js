5f1baf1acdc85227c412260afa8900efaf58c526
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-14 01:12:21 -0300

    will add the image to the initial component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,17 @@
 import styles from "./Home.module.css";
-import img_home from "../../img/img_home.jpg"
+import img_home from "../../img/freedom.jpg"
 
 function Home() {
     return (
         <section className={styles.home_container}>
+            <div className={styles.dialog}>
             <h1>
                 Bem-vindo ao <span>Plane-G!</span>
             </h1>
-            <p>Esteja preparado(a) para imprevistos. Comece planejar suas viagens!</p>
+            <p>Esteja preparado(a) para imprevistos.</p>
+            <p>Comece planejar suas viagens agora!</p>
             <a href="/">Nova viagem</a>
+            <img src={img_home } /></div> 
         </section>
     )
 }
