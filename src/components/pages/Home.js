bd4f084f573a8bea2750e571e9041b53786e196b
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-13 22:20:24 -0300

    will create the base of the home page component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from "./Home.module.css";
+import img_home from "../../img/img_home.jpg"
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>
+                Bem-vindo ao <span>Plane-G!</span>
+            </h1>
+            <p>Esteja preparado(a) para imprevistos. Comece planejar suas viagens!</p>
+            <a href="/">Nova viagem</a>
+        </section>
     )
 }
 
