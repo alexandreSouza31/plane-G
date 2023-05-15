5f4587bbf6bd1bd838ad63d2de2754a443dc1a93
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-15 17:08:59 -0300

    will add the image of unforeseen

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,24 @@
 import styles from "./Home.module.css";
-import img_home from "../../img/freedom.jpg"
+import img_freedom from "../../img/freedom.jpg";
+import img_accident from "../../img/accident.jpg";
 import LinkButton from "../layoult/LinkButton";
 
 function Home() {
     return (
         <section className={styles.home_container}>
-            <div className={styles.dialog}>
             <h1>
                 Bem-vindo ao <span>Plane-G!</span>
             </h1>
-            <p>Esteja preparado(a) para imprevistos.</p>
-            <p>Comece planejar suas viagens agora!</p>
-            <LinkButton to="/newtrip" text="Criar Viagem" />
-            <img src={img_home } /></div> 
+            <span className={styles.span_despair}>
+                <p>Esteja preparado para imprevistos!</p>
+                <img src={img_accident} />
+            </span>
+            <hr></hr>
+            <span className={styles.span_freedom}>
+                <p>Comece planejar suas viagens agora!</p>
+                <LinkButton to="/newtrip" text="Criar Viagem" />
+                <img src={img_freedom} />
+            </span>
         </section>
     )
 }
