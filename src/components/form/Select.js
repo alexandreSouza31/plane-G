e693d212effc2383d93571a9ec82271b2c4f3a8d
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-16 23:17:29 -0300

    will leave the select field marked by default as 'select'

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,7 +5,7 @@ function Select({ text, name, options, handleOnChange, value }) {
         <div className={styles.select_container}>
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
-                <option disabled>Selecione</option>
+                <option selected disabled>Selecione</option>
                 <option>lazer</option>
                 <option>trabalho</option>
             </select>
