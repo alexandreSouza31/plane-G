c43895f94e38681979ffc342a2982e7261792d4f
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-17 19:59:28 -0300

    will dynamically add travel categories via useEffect hook

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,8 +6,12 @@ function Select({ text, name, options, handleOnChange, value }) {
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
                 <option selected disabled>Selecione</option>
-                <option>lazer</option>
-                <option>trabalho</option>
+                {options.map((option) => (
+                    <option value={option.id}
+                        key={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     )
--- src/components/trip/TripForm.js
+++ src/components/trip/TripForm.js
@@ -2,8 +2,25 @@ import styles from "./TripForm.module.css";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
+import { useEffect, useState } from "react";
 
-function TripForm({btnText}) {//btnText vem lá do componente pai, o newTrip
+function TripForm({ btnText }) {//btnText vem lá do componente pai, o newTrip
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() => {
+        //vou precisar fazer um request:
+    fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((resp) => resp.json())//retorna o json
+        .then((data) => {
+            setCategories(data)//reaproveita o dado do json e coloca no hook categories
+        })
+        .catch((err) => console.log(err))//retorna o erro
+    },[])//valor inicial: options vazias
 
     return (
         <form className={styles.form_new_trip}>
@@ -21,8 +38,11 @@ function TripForm({btnText}) {//btnText vem lá do componente pai, o newTrip
                 placeholder="ex: 5000"
                 id="value"
             />
-            <Select name="category_id" text="Selecione a categoria:" />
-            <SubmitButton text={btnText}/>
+            <Select name="category_id"
+                text="Selecione a categoria:"
+                options={categories}
+            />
+            <SubmitButton text={btnText} />
         </form>
     )
 }
