30e7331ff15ff332b3c2860d15bc0b4439146105
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-29 11:15:11 -0300

    will rename the cost data to use

--- src/components/pages/NewTrip.js
+++ src/components/pages/NewTrip.js
@@ -4,25 +4,25 @@ import { useNavigate } from "react-router-dom";
 
 function NewTrip() {
     const navigate = useNavigate()
-    
+
     function createPost(trip) {
         // inicializa o Plane-G! e os serviços
-        trip.planeG = 0
+        trip.cost = 0
         trip.services = []
-        
+
         fetch("http://localhost:5000/trips", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(trip),
-            
+
         }).then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            navigate('/trips', {state:{message : "Viagem criada com sucesso!"}});
-          })  
-        .catch(err=>console.log(err))
+            .then((data) => {
+                console.log(data)
+                navigate('/trips', { state: { message: "Viagem criada com sucesso!" } });
+            })
+            .catch(err => console.log(err))
     }
 
     return (
