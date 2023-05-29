f9be2f029cd8ac4795f5614745077decda60c665
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-29 11:16:27 -0300

    will display the trip information

--- src/components/pages/Trip.js
+++ src/components/pages/Trip.js
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Loading from "../layoult/Loading";
-
+import Container from "../layoult/Container"
 export const Trip = () => {
 
     const { id } = useParams()
     const [trip, setTrip] = useState([])
+    const [showTripForm, setShowTripForm] = useState(false)//mostrará ou não os dados do projeto(inicialmente não mostrará!).
 
     useEffect(() => {
         setTimeout(() => {
@@ -24,9 +25,43 @@ export const Trip = () => {
         }, 3000);
     }, [id])
 
+
+    function toggleTripForm() {//função somente para alterar o estado.
+        setShowTripForm(!showTripForm)
+    }
+
     return (<>
         {trip.name ? (
-            <p>{trip.name}</p>
+            <div>
+                <Container customClass="column">
+                    <div>
+
+                        <h1>Viagem: {trip.name}</h1>
+                        <button onClick={toggleTripForm}>
+                            {!showTripForm ? 'Editar' : 'Fechar'}
+                        </button>
+
+                        {!showTripForm ? (//se for editar...
+                            <div>
+                                <p>
+                                    <span>Categoria: </span>{trip.category.name }
+                                </p>
+                                <p>
+                                    <span>Orçamento Total: </span>{trip.budget }
+                                </p>
+                                <p>
+                                    <span>Total Utilizado: </span>{trip.cost }
+                                </p>
+                            </div>
+                        ) : (//se for ver detalhes
+                            <div>
+                                <p>Detalhes da viagem</p>
+                            </div>
+                        )}
+                    </div>
+                </Container>
+
+            </div>
         ) : (
             <Loading />
         )}
