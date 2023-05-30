26541ffe59b2ddebb42d455ddab97895e2f01d87
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-30 15:40:48 -0300

    will create expenses component

---
+++ src/components/expenses/ExpensesForm.js
@@ -0,0 +1,49 @@
+import styles from "../trip/TripForm.module.css";
+
+import Input from "../form/Input";
+import SubmitButton from "../form/SubmitButton";
+import { useState } from "react";
+
+const ExpensesForm = ({ handleSubmit, btnText, tripData }) => {//props
+    
+    const [expense,setExpense]=useState({})
+
+    function submit(e) {
+        e.preventDefault();
+        tripData.expense.push(expense);
+        handleSubmit(tripData);
+    }
+
+    function handleChange(e) {
+        setExpense({...expense,[e.target.name]:e.target.value})//faço o spread operator pra pegar a despesa atual
+    }
+
+    return (
+        <form action={submit} className={styles.form}>
+            <Input
+                type="text"
+                text="Nome da despesa"
+                name="name"
+                placeholder="digite o nome da despesa"
+                handleOnChange={handleChange}
+            />
+            <Input
+                type="number"
+                text="Valor da despesa"
+                name="cost"
+                placeholder="digite o valor da despesa"
+                handleOnChange={handleChange}
+            />
+            <Input
+                type="text"
+                text="descrição da despesa"
+                name="description"
+                placeholder="descreva a despesa"
+                handleOnChange={handleChange}
+            />
+            <SubmitButton text={btnText } />
+        </form>
+    )
+}
+
+export default ExpensesForm
